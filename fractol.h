a56e59355e52823089dd987a977f57479c1bbc2f
Ismail Lahyani <ilahyani@e2r2p15.1337.ma>
2022-04-22 10:58:21 +0000

    Writing pixels to an image & pushing it to a window

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 13:34:48 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/04/17 15:05:19 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/04/21 15:53:48 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,6 @@
 
 #include "mlx.h"
 #include <stdio.h>
-#include <complex.h>
-
-typedef struct s_color {
-	int	r;
-	int g;
-	int b;
-	int a;
-}	t_color;
 
 typedef struct s_vector
 {
--- main.c
+++ main.c
@@ -1,42 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/15 14:53:51 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/04/17 15:45:12 by ilahyani         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "fractol.h"
 
-void	color_pixel(char *pxl, t_color color)
+void	my_mlx_pixel_put(t_image *data, int x, int y, int color)
 {
-	pxl[0] = color.b;
-	pxl[1] = color.g;
-	pxl[2] = color.r;
-	pxl[3] = color.a;
+	char	*dst;
+
+	dst = data->pixels + (y * data->line_size + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
 }
 
-void	color_img(t_image *img, t_color color)
-{
-	int	x;
-	int	y;
-
-	y = 0;
-	while (y < img->size.y)
-	{
-		x = 0;
-		while (x < img->size.x)
-		{
-			color_pixel(img->pixels[x * 4 + img->line_size * y], color);
-			x++; 
-		}
-		y++;
-	}
-}
+void	draw_square(int	lenght);
+void	draw_circle(void);
+void	draw_triangle(void);
 
 int	main(void)
 {
@@ -45,15 +19,12 @@ int	main(void)
 	t_image img;
 	
 	mlx_pointer = mlx_init();
-	window = mlx_new_window(mlx_pointer, 500, 500, "what do you call a black widow? a window!");
-	img.pointer = mlx_new_image(mlx_pointer, 40, 40);
-	img.size.x = 40;
-	img.size.y = 40;
+	window = mlx_new_window(mlx_pointer, 1000, 1000, "what do you call a black widow? a window!");
+	img.pointer = mlx_new_image(mlx_pointer, 100, 100);
 	img.pixels = mlx_get_data_addr(img.pointer, &img.bits_per_pixel, &img.line_size, &img.endian);
-	mlx_put_image_to_window(mlx_pointer, window, img.pointer, 250, 250);
-	//int		i;
-	// for (i = 0; i < 500; i++)
-	//  	mlx_pixel_put(mlx_pointer, window, i, i, 0xFFFFFF);
-	//mlx_pixel_put(mlx_pointer, img.pointer, 20, 20, 0xFFFFFF);
+	int		i;
+	for(i = 0; i <= 100; i++)
+		my_mlx_pixel_put(&img, 0, i, 0xFFFFFF);
+	mlx_put_image_to_window(mlx_pointer, window, img.pointer, 500, 500);
 	mlx_loop(mlx_pointer);
 }
