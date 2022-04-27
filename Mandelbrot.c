91c132d78581854929aed6a009932497535f8604
Ismail Lahyani <ilahyani@e2r2p15.1337.ma>
2022-04-27 19:14:30 +0000

    Added interactive Julia sets

--- Mandelbrot.c
+++ Mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 18:47:47 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/04/27 19:13:06 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/04/27 19:14:23 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	manderlbrot(t_vars *vars)
 	t_vector	coordinates;
 	t_complex	z_squared;
 	t_complex	z;
+	t_complex	c;
 	
 	coordinates.y = 0;
 	while (coordinates.y <= 500)
@@ -27,20 +28,20 @@ int	manderlbrot(t_vars *vars)
 			vars->itr = 0;
 			z.re = (coordinates.x / 500) * (vars->max_re - vars->min_re) + vars->min_re;
 			z.im = (coordinates.y / 500) * (vars->max_im - vars->min_im) + vars->min_im;
-			vars->c.re = z.re;
-			vars->c.im = z.im;
+			c.re = z.re;
+			c.im = z.im;
 			while (vars->itr < 200 && z.re * z.re + z.im * z.im < 4)
 			{
 				z_squared.re = z.re * z.re - z.im * z.im;
 				z_squared.im = 2 * z.re * z.im;
-				z.re =  z_squared.re + vars->c.re;
-				z.im = z_squared.im + vars->c.im;
+				z.re =  z_squared.re + c.re;
+				z.im = z_squared.im + c.im;
 				vars->itr++;
 			}
 			if (vars->itr == 200)
 				my_mlx_pixel_put(&vars->img, coordinates.x, coordinates.y, create_trgb(0, 0, 0, 0));
 			else
-				my_mlx_pixel_put(&vars->img, coordinates.x, coordinates.y,  create_trgb(1, vars->itr, vars->itr*2, vars->itr*3));
+				my_mlx_pixel_put(&vars->img, coordinates.x, coordinates.y,  create_trgb(1, vars->itr, 1, vars->itr));
 			coordinates.x++;
 		}
 		coordinates.y++;
